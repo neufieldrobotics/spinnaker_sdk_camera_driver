e6db8da281d186aec1fd6f99d1960bd9d22b1419
Joshua Whitley <jwhitley@autonomoustuff.com>
2018-12-06 13:09:05 -0600

    Fixing CMake search process for LibUnwind.

--- cmake/FindLibUnwind.cmake
+++ cmake/FindLibUnwind.cmake
@@ -1,42 +1,42 @@
 # Find the libunwind library
 #
-#  LIBUNWIND_FOUND       - True if libunwind was found.
-#  LIBUNWIND_LIBRARIES   - The libraries needed to use libunwind
-#  LIBUNWIND_INCLUDE_DIR - Location of unwind.h and libunwind.h
+#  LibUnwind_FOUND        - True if libunwind was found.
+#  LibUnwind_LIBRARIES    - The libraries needed to use libunwind
+#  LibUnwind_INCLUDE_DIRS - Location of unwind.h and libunwind.h
 
-FIND_PATH(LIBUNWIND_INCLUDE_DIR libunwind.h)
-if(NOT LIBUNWIND_INCLUDE_DIR)
+FIND_PATH(LibUnwind_INCLUDE_DIRS libunwind.h)
+if(NOT LibUnwind_INCLUDE_DIRS)
   message(STATUS "failed to find libunwind.h")
-elseif(NOT EXISTS "${LIBUNWIND_INCLUDE_DIR}/unwind.h")
+elseif(NOT EXISTS "${LibUnwind_INCLUDE_DIRS}/unwind.h")
   message(STATUS "libunwind.h was found, but unwind.h was not found in that directory.")
-  SET(LIBUNWIND_INCLUDE_DIR "")
+  SET(LibUnwind_INCLUDE_DIRS "")
 endif()
 
-FIND_LIBRARY(LIBUNWIND_GENERIC_LIBRARY "unwind")
-if(NOT LIBUNWIND_GENERIC_LIBRARY)
+FIND_LIBRARY(LibUnwind_GENERIC_LIBRARY "unwind")
+if(NOT LibUnwind_GENERIC_LIBRARY)
     MESSAGE(STATUS "failed to find unwind generic library")
 endif()
-SET(LIBUNWIND_LIBRARIES ${LIBUNWIND_GENERIC_LIBRARY})
+SET(LibUnwind_LIBRARIES ${LibUnwind_GENERIC_LIBRARY})
 
 # For some reason, we have to link to two libunwind shared object files:
 # one arch-specific and one not.
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "^arm")
-    SET(LIBUNWIND_ARCH "arm")
+    SET(LibUnwind_ARCH "arm")
 elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_SYSTEM_PROCESSOR STREQUAL "amd64")
-    SET(LIBUNWIND_ARCH "x86_64")
+    SET(LibUnwind_ARCH "x86_64")
 elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^i.86$")
-    SET(LIBUNWIND_ARCH "x86")
+    SET(LibUnwind_ARCH "x86")
 endif()
 
-if(LIBUNWIND_ARCH)
-    FIND_LIBRARY(LIBUNWIND_SPECIFIC_LIBRARY "unwind-${LIBUNWIND_ARCH}")
-    if (NOT LIBUNWIND_SPECIFIC_LIBRARY)
-        MESSAGE(STATUS "failed to find unwind-${LIBUNWIND_ARCH}")
+if(LibUnwind_ARCH)
+    FIND_LIBRARY(LibUnwind_SPECIFIC_LIBRARY "unwind-${LibUnwind_ARCH}")
+    if (NOT LibUnwind_SPECIFIC_LIBRARY)
+        MESSAGE(STATUS "failed to find unwind-${LibUnwind_ARCH}")
     endif()
-    SET(LIBUNWIND_LIBRARIES ${LIBUNWIND_LIBRARIES} ${LIBUNWIND_SPECIFIC_LIBRARY})
-endif(LIBUNWIND_ARCH)
+    SET(LibUnwind_LIBRARIES ${LibUnwind_LIBRARIES} ${LibUnwind_SPECIFIC_LIBRARY})
+endif(LibUnwind_ARCH)
 
-MARK_AS_ADVANCED(LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIR)
+MARK_AS_ADVANCED(LibUnwind_LIBRARIES LibUnwind_INCLUDE_DIRS)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibUnwind DEFAULT_MSG
-  LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIR)
+  LibUnwind_LIBRARIES LibUnwind_INCLUDE_DIRS)
