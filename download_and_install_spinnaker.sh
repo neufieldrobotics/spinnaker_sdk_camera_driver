b5125f1c3cbfbc0f73c21404c1e4c25c2c6367b6
Vikrant Shah <shah.vi@husky.neu.edu>
2020-06-26 13:15:20 -0400

    Feature/dev ci (#91)
    * Adding travis ci and install script
    
    * Update .travis.yml
    
    * Making wget silent and adding shields to readme
    
    * adding ci test matrix
    
    * debugging ci matrix
    
    * debug ci matrix
    
    * debug ci matrix
    
    * debug ci matrix
    
    * Adding multiple versions of spinnaker
    
    * fixing type
    
    * naming style between spinnaker deb versions
    
    Co-authored-by: auv <vik748@hulkd>

--- download_and_install_spinnaker.sh
+++ download_and_install_spinnaker.sh
@@ -4,39 +4,21 @@ set -x
 CWD=`pwd`
 
 export DEBIAN_FRONTEND=noninteractive
-export ROS_DISTRO kinetic
-export SPINNAKER_VERSION=2.0.0.147
-export SPINNAKER_LINUX_ARCH=amd64
 
 # install basic packages
 apt-get update
 apt-get install -q -y --no-install-recommends \
     build-essential tree wget dirmngr gnupg2 vim nano git debconf-utils libunwind-dev
 
-wget https://www.dl.dropboxusercontent.com/s/xq5f6r15i4rea4w/spinnaker-$SPINNAKER_VERSION-amd64-pkg.tar.gz -nv
+wget https://coe.northeastern.edu/fieldrobotics/spinnaker_sdk_archive/spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH-pkg.tar.gz -nv
 
 tar -zxvf spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH-pkg.tar.gz
-#rm spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH-pkg.tar.gz
 cd spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH
 
+# auto accept spinnaker license agreements
 echo libspinnaker libspinnaker/present-flir-eula note '' | debconf-set-selections
 echo libspinnaker libspinnaker/accepted-flir-eula boolean true | debconf-set-selections
 
-dpkg -i   libspinnaker_*.deb
-dpkg -i   libspinnaker-dev_*.deb
-dpkg -i   libspinnaker-c_*.deb
-dpkg -i   libspinnaker-c-dev_*.deb
-dpkg -i   libspinvideo_*.deb
-dpkg -i   libspinvideo-dev_*.de
-dpkg -i   libspinvideo-c_*.deb
-dpkg -i   libspinvideo-c-dev_*.deb
-dpkg -i   spinview-qt_*.deb
-dpkg -i   spinview-qt-dev_*.deb
-dpkg -i   spinupdate_*.deb
-dpkg -i   spinupdate-dev_*.deb
-dpkg -i   spinnaker_*.deb
-dpkg -i   spinnaker-doc_*.deb
+dpkg -i   *.deb
 
 cd ..
-#rm -r spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH/
-
