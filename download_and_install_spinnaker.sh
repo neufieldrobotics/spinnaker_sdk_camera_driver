d499f67231e3b52fbc09b9efedc6ab41a7314b4c
auv <vik748@hulkd>
2020-06-26 12:37:47 -0400

    naming style between spinnaker deb versions

--- download_and_install_spinnaker.sh
+++ download_and_install_spinnaker.sh
@@ -22,20 +22,7 @@ cd spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH
 echo libspinnaker libspinnaker/present-flir-eula note '' | debconf-set-selections
 echo libspinnaker libspinnaker/accepted-flir-eula boolean true | debconf-set-selections
 
-dpkg -i   libspinnaker_*.deb
-dpkg -i   libspinnaker-dev_*.deb
-dpkg -i   libspinnaker-c_*.deb
-dpkg -i   libspinnaker-c-dev_*.deb
-dpkg -i   libspinvideo_*.deb
-dpkg -i   libspinvideo-dev_*.de
-dpkg -i   libspinvideo-c_*.deb
-dpkg -i   libspinvideo-c-dev_*.deb
-dpkg -i   spinview-qt_*.deb
-dpkg -i   spinview-qt-dev_*.deb
-dpkg -i   spinupdate_*.deb
-dpkg -i   spinupdate-dev_*.deb
-dpkg -i   spinnaker_*.deb
-dpkg -i   spinnaker-doc_*.deb
+dpkg -i   *.deb
 
 cd ..
 #rm -r spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH/
