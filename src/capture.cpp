4e5f125e33d06e1d5499601192446376ee249c39
Pushyami Kaveti <kaveti.p@husky.neu.edu>
2020-07-01 22:06:58 -0400

    Feature/error handling (#57)
    * handling ros shutdown to exit the node gracefully
    
    * Fixing rebase conflicts
    
    * Little stuff to match with dev
    
    Co-authored-by: Vikrant Shah <vikrantshah@gmail.com>

--- src/capture.cpp
+++ src/capture.cpp
@@ -23,7 +23,7 @@ acquisition::Capture::~Capture(){
 
     ROS_INFO_STREAM("Releasing camera pointers...");
     cams.clear();
-    
+
     ROS_INFO_STREAM("Releasing system instance...");
     system_->ReleaseInstance();
 
@@ -37,8 +37,6 @@ acquisition::Capture::~Capture(){
     pubThread_.reset();
     //reset it_
     it_.reset();
-
-    ros::shutdown();
 
 }
 
@@ -1064,6 +1062,8 @@ void acquisition::Capture::run_soft_trig() {
     catch(...){
         ROS_FATAL_STREAM("Some unknown exception occured. \v Exiting gracefully, \n  possible reason could be Camera Disconnection...");
     }
+    ros::shutdown();
+    //raise(SIGINT);
 }
 
 float acquisition::Capture::mem_usage() {
